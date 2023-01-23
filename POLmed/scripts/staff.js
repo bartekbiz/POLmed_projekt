ad792179efe468a222ccde60c1fbfde2a4916d17
Czcibor Bohusz-Dobosz <czbd@o2.pl>
2023-01-23 22:25:06 +0100

    Moar json

--- POLmed/scripts/staff.js
+++ POLmed/scripts/staff.js
@@ -13,18 +13,22 @@ fetch("./data/staff.json")
 
 		//I know, I know, not proud of this code but time constraints
 
+		let nlekarzy = 0;
+		let npoloznych = 0;
+
 		for(var i in data.lekarze) {
 			trlekarze.innerHTML += '<div class="col-md-4 p-3"> \
 				<div class="card"> \
 					<div class="card-body"> \
 						<img class="w-100" src="' + data.lekarze[i].img + '" alt=""/> \
 						<h5 class="card-title pt-2">' + data.lekarze[i].name + '</h5> \
-						<h6 class="card-subtitle mb-2 text-muted">' + data.lekarze[i].profession + '</h6> \
+						<h6 class="card-subtitle mb-2 text-muted">' + data.lekarze[i].spec + '</h6> \
 						<p class="card-text">' + data.lekarze[i].desc + '</p> \
-						<a href="booking.html?id=' + data.lekarze[i].id + '" class="btn btn-primary w-100">Zarezerwuj wizytę</a> \
+						<a href="booking.html?id=' + i + '" class="btn btn-primary w-100">Zarezerwuj wizytę</a> \
 					</div> \
 				</div> \
 			</div>';
+			nlekarzy++;
 		}
 
 		for(var i in data.polozne) {
@@ -33,12 +37,13 @@ fetch("./data/staff.json")
 					<div class="card-body"> \
 						<img class="w-100" src="' + data.polozne[i].img + '" alt=""/> \
 						<h5 class="card-title pt-2">' + data.polozne[i].name + '</h5> \
-						<h6 class="card-subtitle mb-2 text-muted">' + data.polozne[i].profession + '</h6> \
+						<h6 class="card-subtitle mb-2 text-muted">' + data.polozne[i].spec + '</h6> \
 						<p class="card-text">' + data.polozne[i].desc + '</p> \
-						<a href="booking.html?id=' + data.polozne[i].id + '" class="btn btn-primary w-100">Zarezerwuj wizytę</a> \
+						<a href="booking.html?id=' + parseInt(parseInt(nlekarzy)+parseInt(i)) + '" class="btn btn-primary w-100">Zarezerwuj wizytę</a> \
 					</div> \
 				</div> \
 			</div>';
+			npoloznych++;
 		}
 
 		for(var i in data.pielegniarze) {
@@ -47,9 +52,9 @@ fetch("./data/staff.json")
 					<div class="card-body"> \
 						<img class="w-100" src="' + data.pielegniarze[i].img + '" alt=""/> \
 						<h5 class="card-title pt-2">' + data.pielegniarze[i].name + '</h5> \
-						<h6 class="card-subtitle mb-2 text-muted">' + data.pielegniarze[i].profession + '</h6> \
+						<h6 class="card-subtitle mb-2 text-muted">' + data.pielegniarze[i].spec + '</h6> \
 						<p class="card-text">' + data.pielegniarze[i].desc + '</p> \
-						<a href="booking.html?id=' + data.pielegniarze[i].id + '" class="btn btn-primary w-100">Zarezerwuj wizytę</a> \
+						<a href="booking.html?id=' + parseInt(parseInt(nlekarzy)+parseInt(npoloznych)+parseInt(i)) + '" class="btn btn-primary w-100">Zarezerwuj wizytę</a> \
 					</div> \
 				</div> \
 			</div>';
