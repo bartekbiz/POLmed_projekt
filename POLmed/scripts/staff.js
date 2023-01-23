b30e6535320ac2daa00969d68213f9d68312493d
Czcibor Bohusz-Dobosz <czbd@o2.pl>
2023-01-23 21:57:00 +0100

    More turning stuff into json

--- POLmed/scripts/staff.js
+++ POLmed/scripts/staff.js
@@ -1,17 +1,67 @@
-fetch("../data/staff.json")
-  .then((res) => res.json())
-  .then((data) => {
-    // do stuff with the data
-    console.log(data);
-  });
+fetch("./data/staff.json")
+	.then((res) => res.json())
+	.then((data) => {
+		const trwszyscy = document.querySelector("#trwszyscy");
+		const trlekarze = document.querySelector("#trlekarze");
+		const trpielegniarze = document.querySelector("#trpielegniarze");
+		const trpolozne = document.querySelector("#trpolozne");
+
+		trwszyscy.innerHTML = "";
+		trlekarze.innerHTML = "";
+		trpolozne.innerHTML = "";
+		trpielegniarze.innerHTML = "";
+
+		//I know, I know, not proud of this code but time constraints
+
+		for(var i in data.lekarze) {
+			trlekarze.innerHTML += '<div class="col-md-4 p-3"> \
+				<div class="card"> \
+					<div class="card-body"> \
+						<img class="w-100" src="' + data.lekarze[i].img + '" alt=""/> \
+						<h5 class="card-title pt-2">' + data.lekarze[i].name + '</h5> \
+						<h6 class="card-subtitle mb-2 text-muted">' + data.lekarze[i].profession + '</h6> \
+						<p class="card-text">' + data.lekarze[i].desc + '</p> \
+						<a href="booking.html?id=' + data.lekarze[i].id + '" class="btn btn-primary w-100">Zarezerwuj wizytę</a> \
+					</div> \
+				</div> \
+			</div>';
+		}
+
+		for(var i in data.polozne) {
+			trpolozne.innerHTML += '<div class="col-md-4 p-3"> \
+				<div class="card"> \
+					<div class="card-body"> \
+						<img class="w-100" src="' + data.polozne[i].img + '" alt=""/> \
+						<h5 class="card-title pt-2">' + data.polozne[i].name + '</h5> \
+						<h6 class="card-subtitle mb-2 text-muted">' + data.polozne[i].profession + '</h6> \
+						<p class="card-text">' + data.polozne[i].desc + '</p> \
+						<a href="booking.html?id=' + data.polozne[i].id + '" class="btn btn-primary w-100">Zarezerwuj wizytę</a> \
+					</div> \
+				</div> \
+			</div>';
+		}
+
+		for(var i in data.pielegniarze) {
+			trpielegniarze.innerHTML += '<div class="col-md-4 p-3"> \
+				<div class="card"> \
+					<div class="card-body"> \
+						<img class="w-100" src="' + data.pielegniarze[i].img + '" alt=""/> \
+						<h5 class="card-title pt-2">' + data.pielegniarze[i].name + '</h5> \
+						<h6 class="card-subtitle mb-2 text-muted">' + data.pielegniarze[i].profession + '</h6> \
+						<p class="card-text">' + data.pielegniarze[i].desc + '</p> \
+						<a href="booking.html?id=' + data.pielegniarze[i].id + '" class="btn btn-primary w-100">Zarezerwuj wizytę</a> \
+					</div> \
+				</div> \
+			</div>';
+		}
+
+		trwszyscy.innerHTML += trlekarze.innerHTML;
+		trwszyscy.innerHTML += trpolozne.innerHTML;
+		trwszyscy.innerHTML += trpielegniarze.innerHTML;
+	});
 
 const lekarzeContainer = document.querySelector("#twszyscy");
 
 if(window.location.hash == "#lekarze" || window.location.hash == "#pielegniarze" || window.location.hash == "#polozne") {
-    bootstrap.Tab.getOrCreateInstance(document.querySelector(window.location.hash)).show();
-}
-document.onclick = function(e) {
-    if(e.target.classList.contains("bookme")) {
-        window.open("booking.html?lekarz=" + e.target.parentNode.children[1].innerText + "&spec=" + e.target.parentNode.children[2].innerText, "_self");
-    }
+		bootstrap.Tab.getOrCreateInstance(document.querySelector(window.location.hash)).show();
 }
